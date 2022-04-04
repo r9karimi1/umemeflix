54ce3b29034203ab8ba944cf1e4bee9c02e4c626
BabushkaBoi <70940998+r9karimi1@users.noreply.github.com>
2022-04-04 11:19:46 -0400

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,45 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="images/logo1.png" height="50"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+
+             <!-- Categories Widget -->
+             
+          <div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+<?php $query=mysqli_query($con,"select id,CategoryName from tblcategory");
+while($row=mysqli_fetch_array($query))
+{
+?>
+
+                    <li class="menu-arrow">
+                      <a href="category.php?catid=<?php echo htmlentities($row['id'])?>"><?php echo htmlentities($row['CategoryName']);?></a>
+                    </li>
+<?php } ?>
+                  </ul>
+                </div>
+       
+              </div>
+            </div>
+          </div>
+
+                 <li class="nav-item">
+              <a class="nav-link" href="index.php">News</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">Contact us</a>
+            </li>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
